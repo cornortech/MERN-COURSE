f75453a829e4764f4c1ad02ed8c0182de8c0b626
Ramit Neupane <ramitnpn986@gmail.com>
2025-05-12 21:34:54 +0545

    functions and loops done

---
+++ frontend/javascript/function/index.js
@@ -0,0 +1,75 @@
+// FUNCTION IN JS
+// WHAT IS A FUNCTION?
+
+// A function is a block of code that performs a specific task. { }
+// It is a reusable piece of code that can be called multiple times with different inputs.
+// Functions can take parameters (inputs) and return a value (output).
+// Functions are used to organize code, make it more readable, and avoid repetition.
+
+
+
+// let a = 10;
+// let b = 20;
+// let sum = a + b;
+
+// let c = 1000;
+// let d = 2000;
+// console.log(c + d)
+
+
+// let e = 5000;
+// let f = 5000;
+
+// console.log(e + f); // 3000
+
+
+
+// keywords / reserved words - the words that are reserved by the language 
+
+
+
+
+
+// function declaration / function definition
+
+// function addition(number1 , number2 ){
+
+//  let add = number1 + number2;
+
+//  console.log(add);
+
+// }
+
+
+// addition(10 , 20) // 30
+    
+
+// function multiply(num1 ,num2){
+
+//     console.log("Number 1 is :", num1);
+//     console.log("Number 2 is :", num2);
+
+//     let multiplyResult = num1 * num2;
+//     console.log("Multiplication is :" , multiplyResult);
+// }
+
+
+// multiply(10 , 20)
+// multiply(20 , 30)
+
+
+
+function greet(name){
+    console.log("Namaste " + name);
+    console.log("Hare Krishna " + name);
+}
+
+
+greet("Ashwin")
+greet("Sushant")
+greet("Maikal")
+greet("Gajendra Sharma")
+greet("Rajesh")
+greet("Abhay")
+greet("Aman")
+
---
+++ frontend/javascript/loops/index.js
@@ -0,0 +1,63 @@
+
+// loops in js 
+// 1. for loop
+
+// initialization, condition, execution ,  increment
+
+// i = 0
+// i = 1
+// i = 2
+// i = 3
+// i = 4
+// i = 5
+
+// for (let i = 6; i < 5; i++) {
+
+
+//     console.log(i);
+
+
+// }
+
+
+
+
+
+
+
+// 2. while loop
+// dont execute the code if the condition is false
+
+
+// let i = 6;
+
+
+// // jaba 
+// while ( i < 5){
+//     console.log(i);
+//     i++;
+// }
+
+
+
+
+
+
+// 3. do while loop
+
+
+
+// let i = 0 ;
+
+// do {
+
+//     console.log(i);
+//     i++;
+
+// } while (i < 5);
+
+
+// difference between while and do while loop
+
+// 1. while loop checks the condition first and then executes the code
+//  but do while loop executes the code first and then checks the condition
